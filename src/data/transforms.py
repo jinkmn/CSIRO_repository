7275d9d5784700bc19444f24134614cc567825ef
jin <jintakahama0821@icloud.com>
2025-12-10 21:19:07 +0900

    simple head

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -59,68 +59,24 @@ class TransformFactory:
 
         return [original, hflip, vflip]
 
-class BaseTransform:
-    """
-    Transformの基底クラス
-    __call__ を実装することで、インスタンスを関数のように呼べるようにする
-    """
-    def __init__(self):
-        self.transform = A.Compose([])
+class BiomassTransformFactory:
+    def __init__(self, img_size: int):
+        self.img_size = img_size
 
-    def __call__(self, **kwargs):
-        # Dataset内で transform(image=img) と呼ばれたときに動く
-        return self.transform(**kwargs)
-
-class TrainTransform(BaseTransform):
-    def __init__(self, img_size: int = 224, aug_prob: float = 0.5):
-        self.transform = A.Compose([
-            A.Resize(img_size, img_size),
+    def get_train_transforms(self):
+        return A.Compose([
+            A.Resize(self.img_size, self.img_size),
+            A.RandomRotate90(p=0.5),
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
-            
-            # 幾何学的変換
-            A.ShiftScaleRotate(
-                shift_limit=0.1, 
-                scale_limit=0.1, 
-                rotate_limit=30, 
-                border_mode=cv2.BORDER_REFLECT, 
-                p=aug_prob
-            ),
-            
-            # 色調変化
-            A.OneOf([
-                A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.8),
-                A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.8),
-            ], p=aug_prob),
-            
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.GaussianBlur(blur_limit=(5, 5), sigma_limit=0, p=0.5),
+            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2(),
         ])
 
-class ValidTransform(BaseTransform):
-    def __init__(self, img_size: int = 224):
-        self.transform = A.Compose([
-            A.Resize(img_size, img_size),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    def get_valid_transforms(self):
+        return A.Compose([
+            A.Resize(self.img_size, self.img_size),
+            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2(),
         ])
-
-class InferenceTransform(BaseTransform):
-    def __init__(self, img_size: int = 224, tta_idx: int = 0):
-        """
-        tta_idx: 0=Original, 1=HorizontalFlip, 2=VerticalFlip
-        """
-        transforms = [A.Resize(img_size, img_size)]
-        
-        # TTAロジック
-        if tta_idx == 1:
-            transforms.append(A.HorizontalFlip(p=1.0))
-        elif tta_idx == 2:
-            transforms.append(A.VerticalFlip(p=1.0))
-            
-        transforms.extend([
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ])
-        
-        self.transform = A.Compose(transforms)
