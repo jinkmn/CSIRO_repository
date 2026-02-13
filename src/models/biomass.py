b0e988c588b29df48883b77574097c7eb6104028
jin <jintakahama0821@icloud.com>
2026-02-13 21:50:50 +0900

    add transform

--- src/models/biomass.py
+++ src/models/biomass.py
@@ -95,7 +95,8 @@ class BiomassModel(nn.Module):
         pretrained: bool = True, 
         out_dim: int = 3,  
         drop_rate: float = 0.3,
-        freeze: bool = True
+        freeze: bool = True,
+        img_size: int = 512
     ):
         """
         Args:
@@ -111,6 +112,7 @@ class BiomassModel(nn.Module):
             model_name,
             pretrained=pretrained,
             num_classes=0,
+            img_size=img_size
         )
 
         for param in self.backbone.parameters():
